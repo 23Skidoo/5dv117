3ab0009dface12fcbf10362125058866543c59f1
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2013-11-03 17:54:47 +0100

    Demonstration programs stubs.

--- src/exe/decisionAlgorithm/Main.hs
+++ src/exe/decisionAlgorithm/Main.hs
@@ -1,5 +1,14 @@
 module Main
        where
 
+import System.Environment (getArgs)
+
+demo :: IO ()
+demo = putStrLn "Not implemented."
+
 main :: IO ()
-main = putStrLn "Not implemented."
+main = do
+  args <- getArgs
+  if length args < 2
+    then demo
+    else putStrLn "Not implemented."
